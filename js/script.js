fe426a73dcf50369b6282d2212bf1e31b0965924
Abel Meseret <abelmeseret@Abels-MacBook-Pro.local>
2024-04-27 04:56:33 +0300

    Instantiatd Tamagotchi (updated class and pseudo code)

--- js/script.js
+++ js/script.js
@@ -4,11 +4,12 @@
 
 // Create an Object or Class (look at your notes on JS Classes if you forgot) for your Tamagotchi
 class Pet {
-    constructor(hunger, sleepiness, boredom, age  ) {
+    constructor(hunger, sleepiness, boredom, age, name ) {
           this.hunger = hunger
           this.sleepiness = sleepiness
           this.boredom = boredom
           this.age = age
+          this.name = name
     }
 
     eat() {
@@ -32,16 +33,22 @@ class Pet {
 
 // Instantiate your Tamagotchi (If you created a class)
 
+const firstPet = new Pet(1, 1, 1, 0, "baby lion");
+
 // Display a character of your choice on the screen to represent your pet
+//-------- I already have the character displayed
 
 // Display the following metrics for your pet:
+//------- I have the metrics displayed in html and css now I have to make them active in javascript
 
 // Hunger (1-10 scale)
 // Sleepiness (1-10 scale)
 // Boredom (1-10 scale)
 // Age
 // Add buttons to the screen to feed your pet, 
+//----------1. I have the buttons displayed in html and css now I have to make them active in javascript
 // turn off the lights, 
+//-----1. these two buttons were the first button I made active in javascript 
 document.addEventListener('DOMContentLoaded', function() {
     const lightsOnBtn = document.querySelector('.lights-on');
     const lightsOffBtn = document.querySelector('.lights-off');
@@ -69,6 +76,7 @@ document.addEventListener('DOMContentLoaded', function() {
 // Add the ability to name your pet.
 
 // Style the page.
+//------1. Some styling is already done I might update it more later in the build
 
 // Increase your pet's age every x minutes
 
@@ -77,6 +85,7 @@ document.addEventListener('DOMContentLoaded', function() {
 // You pet should die if Hunger, Boredom, or Sleepiness hits 10.
 
 // Morph your pet at certain ages.
+//------1. I have the different versions of my pet already written in html and css I just have to add the morphing feature in javascript
 
 // Animate your pet across the screen while it's alive.
 
